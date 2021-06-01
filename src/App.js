cbd7c020a0d9773906b8eae70187626c5c03b8b3
Praga <praghatieshss.topnotch@gmail.com>
2021-06-01 12:38:37 +0530

    linkedin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Portfolio from "./component/Portfolio"
 export default  function App() {
+  document.title="portfolio"
   return (
+    
     <div>
       <Portfolio/>
     </div>
