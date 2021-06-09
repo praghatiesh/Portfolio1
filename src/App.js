001c7ce6628c03ca172f29f0a456f5d162bc1bd0
PRAGHATIESH S <73994558+praghatiesh@users.noreply.github.com>
2021-06-09 11:10:40 +0530

    Update App.js
    new update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Portfolio from "./component/Portfolio"
-export default  function App() {
-//   document.title="portfolio";
+import Project from "./component/Project";
+import Home from "./component/Home";
+import Footer from "./component/Footer";
+import Skills from "./component/Skills";
+import Nav from "./component/Nav";
+import About from "./component/About"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+export default function App() {
+  document.title = "portfolio";
   return (
-    <div>      
-      <Portfolio/>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Skills" component={Skills} />
+          <Route path="/About" component={About}/>
+          <Route path="/Project" component={Project} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-
