d74238e5fc0e5c324a1d06b8429a8966e939f91b
PRAGHATIESH S <73994558+praghatiesh@users.noreply.github.com>
2022-05-15 14:45:51 +0530

    new aws

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -4,12 +4,14 @@ export default function Skills() {
     <div>
       <h1 style={{ textAlign: "center", marginBottom: 50 }}>Skills</h1>
       <div className="skill">
+        <div className="skillbox">+Aws</div>
+        <div className="skillbox">+Sql</div>
+        <div className="skillbox">+Python</div>
         <div className="skillbox">+Reactjs</div>
-        <div className="skillbox">+javascript</div>
         <div className="skillbox">+HTML</div>
         <div className="skillbox">+CSS</div>
         <div className="skillbox">+Java</div>
-        <div className="skillbox">+Mysql</div>
+        
       </div>
     </div>
   );
